859a3016e51f89c1e82796505dc6511cb543165e
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-09-14 19:48:28 -0400

    fully functional post listing yes yes yes

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,16 +1,11 @@
 from django.db import models
 
 class Listing(models.Model):
-    # Columns of the SQLite (soon to be PostgreSQL) database are defined here
-    owner = models.CharField(max_length=20, default='admin')
-    itemName = models.CharField(max_length=40, default='An Item')
-    itemCategory = models.CharField(max_length=40, default='A Category')
-    school = models.CharField(max_length=80, default='School')
-    description = models.CharField(max_length=1000, default='Description')
-    price = models.PositiveIntegerField(default='0')
-    
-    # Temporarily replace ArrayField with TextField for SQLite compatibility
-    imageURLs = models.TextField(blank=True, default='')  # Store URLs as a comma-separated string
-
-    def __str__(self):
-        return self.itemName
+    owner = models.CharField(max_length=100)
+    itemName = models.CharField(max_length=100)
+    itemCategory = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    imageURLs = models.TextField()  # Storing the AWS URLs as a comma-separated string
+    active = models.BooleanField(default=True)  # represents whether listing is active or not
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,9 +46,15 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
