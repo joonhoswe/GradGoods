0e2b09a2fe51251d64e0d76e7766a03697349cd7
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-09-14 14:00:35 -0400

    send input to buy page

--- src/pages/buy.jsx
+++ src/pages/buy.jsx
@@ -1,8 +1,18 @@
 import { React, useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 
 export default function Buy()
 {
-
+    const location = useLocation();
+    const { inputValue } = location.state || {};
+  
+    return (
+      <div>
+        <h1>Second Page</h1>
+        <p>Received Input: {inputValue}</p>
+      </div>
+    );
 }
