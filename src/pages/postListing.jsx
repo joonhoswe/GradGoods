0473b0f4ed7e2bc0eb2c39323d8c763fcf7f7719
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-09-15 00:26:42 -0400

    disabled fields for invalid email

--- src/pages/postListing.jsx
+++ src/pages/postListing.jsx
@@ -129,157 +129,154 @@ export default function postListing() {
     };
 
     return (
-    <>
-        <Navbar/>
-        
-        {!isEmailValid && 
-            <div className="flex items-center justify-center w-full bg-red-500 text-white p-4 font-bold">
-                You MUST have a valid .edu email address to post a listing!
-            </div>
-        }
-        
-        
-        <div className='h-full w-full lg:p-32 flex justify-center items-center'>
-            <div className='flex flex-row h-full w-full'>
-                {/* left side of page */}
-                <div className='flex flex-col text-3xl font-bold h-full w-full md:w-1/2'>
-                    <h1 > List an Item </h1>
-                    {/* image upload box */}
-                    <div className="flex flex-col"  style = {{paddingTop: '30px'}}>
-                        {/* Make Image Input */}
-                        <div className="flex items-center">
-                            <div
-                                className="flex items-center justify-center h-8 w-8 bg-green-700 text-white rounded-full mr-4"
-                                style={{ fontSize: '14px' }}
-                            >
-                                1
+        <>
+            <Navbar/>
+            
+            {!isEmailValid && 
+                <div className="flex items-center justify-center w-full bg-red-500 text-white p-4 font-bold">
+                    You MUST have a valid .edu email address to post a listing!
+                </div>
+            }
+            
+            <div className='h-full w-full lg:p-32 flex justify-center items-center'>
+                <div className='flex flex-row h-full w-full'>
+                    {/* left side of page */}
+                    <div className='flex flex-col text-3xl font-bold h-full w-full md:w-1/2'>
+                        <h1> List an Item </h1>
+                        {/* image upload box */}
+                        <div className="flex flex-col" style={{paddingTop: '30px'}}>
+                            <div className="flex items-center">
+                                <div className="flex items-center justify-center h-8 w-8 bg-green-700 text-white rounded-full mr-4" style={{fontSize: '14px'}}>
+                                    1
+                                </div>
+                                <label className="font-medium" style={{paddingLeft: '0px', paddingBottom: '1px', fontSize: '20px'}}>
+                                    Image Upload
+                                </label>
                             </div>
-                            <label
-                                className="font-medium"
-                                style={{ paddingLeft: '0px', paddingBottom: '1px', fontSize: '20px'}}
-                            >
-                                Image Upload
-                            </label>
-                        </div>
-                        <div className="flex items-center justify-center h-72 w-full max-w-lg rounded-lg border-2 border-grey mt-3 relative" style={{ width: '500px', paddingTop: '5px'}}>
-                            <input
-                            className="absolute opacity-0 w-full h-full cursor-pointer"
-                            type="file"
-                            multiple
-                            accept="image/*"
-                            onChange={handleFileChange}
-                            />
-                            <div className="flex flex-col items-center justify-center">
-                                <img src={upload} alt='upload' className='h-24 w-24'/>
-                                <p className="mt-2 text-gray-500" style={{fontSize:'20px'}}> Drag & Drop your images here, or </p>
-                                <button className="mt-2 text-blue-500 underline" style={{fontSize:'20px'}}> Choose File </button>
-                            </div>
-                        </div>
-
-                        {/* each image uploaded */}
-                        <div className="mt-4 w-full">
-                            {fileNames.map((name, index) => (
-                                <div key={index} className="w-full h-8 flex items-center justify-between rounded-lg bg-gray-200 p-2 mb-2">
-                                    <p className="text-blue-500">{name}</p>
-                                    <button onClick={() => handleFileDelete(index)} className="text-red-500 text-sm hover:text-gray-400 transition ease-in-out duration-300" title='Remove Image'> 
-                                        x 
+                            <div className={`flex items-center justify-center h-72 w-full max-w-lg rounded-lg border-2 border-grey mt-3 relative ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-pointer'}`} style={{width: '500px', paddingTop: '5px'}}>
+                                <input
+                                    className="absolute opacity-0 w-full h-full"
+                                    type="file"
+                                    multiple
+                                    accept="image/*"
+                                    onChange={handleFileChange}
+                                    disabled={!isEmailValid} // Disable input if the email is not valid
+                                />
+                                <div className="flex flex-col items-center justify-center">
+                                    <img src={upload} alt='upload' className='h-24 w-24'/>
+                                    <p className="mt-2 text-gray-500" style={{fontSize: '20px'}}> Drag & Drop your images here, or </p>
+                                    <button className={`mt-2 text-blue-500 underline ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-pointer'}`} disabled={!isEmailValid}>
+                                        Choose File
                                     </button>
                                 </div>
-                            ))}
+                            </div>
+    
+                            {/* each image uploaded */}
+                            <div className="mt-4 w-full">
+                                {fileNames.map((name, index) => (
+                                    <div key={index} className="w-full h-8 flex items-center justify-between rounded-lg bg-gray-200 p-2 mb-2">
+                                        <p className="text-blue-500">{name}</p>
+                                        <button onClick={() => handleFileDelete(index)} className={`text-red-500 text-sm hover:text-gray-400 transition ease-in-out duration-300 ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-pointer'}`} title='Remove Image' disabled={!isEmailValid}>
+                                            x
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                {/* right side of page */}
-                <div className='flex flex-col h-full w-1/2 space-y-4'>
-
-                    {/* Item Name */}
-                    <div className="flex flex-col">
-                        <label className="text-lg font-medium" htmlFor="itemName">Item Name</label>
-                        <input 
-                            id="itemName" 
-                            type="text" 
-                            value={itemName} 
-                            onChange={(e) => setItemName(e.target.value)} 
-                            placeholder="Enter item name" 
-                            className="p-2 border rounded-lg"
-                        />
-                    </div>
-
-                    {/* Item Category */}
-                    <div className="flex flex-col">
-                        <label className="text-lg font-medium" htmlFor="itemCategory">Item Category</label>
-                        <input 
-                            id="itemCategory" 
-                            type="text" 
-                            value={itemCategory} 
-                            onChange={(e) => setItemCategory(e.target.value)} 
-                            placeholder="Enter item category" 
-                            className="p-2 border rounded-lg"
-                        />
-                    </div>
-
-                    {/* School */}
-                    <div className="flex flex-col">
-                        <label className="text-lg font-medium" htmlFor="school">School</label>
-                        <input 
-                            id="school" 
-                            type="text" 
-                            value={school} 
-                            onChange={(e) => setSchool(e.target.value)} 
-                            placeholder="Enter school name" 
-                            className="p-2 border rounded-lg"
-                        />
-                    </div>
-
-                    {/* Description */}
-                    <div className="flex flex-col">
-                        <label className="text-lg font-medium" htmlFor="description">Description</label>
-                        <textarea 
-                            id="description" 
-                            value={description} 
-                            onChange={(e) => setDescription(e.target.value)} 
-                            placeholder="Enter item description" 
-                            className="p-2 border rounded-lg"
-                            rows="4"
-                        />
-                    </div>
-
-                    {/* Price */}
-                    <div className="flex flex-col">
-                        <label className="text-lg font-medium" htmlFor="price">Price</label>
-                        <div className='flex flex-row items-center'>
-                            <p className='text-lg font-medium'>$</p>
+    
+                    {/* right side of page */}
+                    <div className='flex flex-col h-full w-1/2 space-y-4'>
+    
+                        {/* Item Name */}
+                        <div className="flex flex-col">
+                            <label className="text-lg font-medium" htmlFor="itemName">Item Name</label>
                             <input 
-                            id="price" 
-                            type="number" 
-                            value={price} 
-                            onChange={(e) => setPrice(e.target.value === '' ? '' : parseInt(e.target.value))}
-                            onKeyDown={(e) => {
-                                if (!/[0-9]/.test(e.key) && e.key !== 'Backspace' && e.key !== 'Delete' && e.key !== 'ArrowLeft' && e.key !== 'ArrowRight' && e.key !== 'Tab') {
-                                    e.preventDefault();
-                                }
-                            }}
-                            placeholder="Enter item price" 
-                            className="p-2 border rounded-lg"
-                        />
+                                id="itemName" 
+                                type="text" 
+                                value={itemName} 
+                                onChange={(e) => setItemName(e.target.value)} 
+                                placeholder="Enter item name" 
+                                className={`p-2 border rounded-lg ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-text'}`}
+                                disabled={!isEmailValid} // Disable input if the email is not valid
+                            />
                         </div>
-                        
+    
+                        {/* Item Category */}
+                        <div className="flex flex-col">
+                            <label className="text-lg font-medium" htmlFor="itemCategory">Item Category</label>
+                            <input 
+                                id="itemCategory" 
+                                type="text" 
+                                value={itemCategory} 
+                                onChange={(e) => setItemCategory(e.target.value)} 
+                                placeholder="Enter item category" 
+                                className={`p-2 border rounded-lg ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-text'}`}
+                                disabled={!isEmailValid} // Disable input if the email is not valid
+                            />
+                        </div>
+    
+                        {/* Price */}
+                        <div className="flex flex-col">
+                            <label className="text-lg font-medium" htmlFor="price">Price</label>
+                            <div className='flex flex-row items-center'>
+                                <p className='text-lg font-medium'>$</p>
+                                <input 
+                                    id="price" 
+                                    type="number" 
+                                    value={price} 
+                                    onChange={(e) => setPrice(e.target.value === '' ? '' : parseInt(e.target.value))}
+                                    placeholder="Enter item price" 
+                                    className={`p-2 border rounded-lg ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-text'}`}
+                                    disabled={!isEmailValid} // Disable input if the email is not valid
+                                />
+                            </div>
+                        </div>
+    
+                        {/* School */}
+                        <div className="flex flex-col">
+                            <label className="text-lg font-medium" htmlFor="school">School</label>
+                            <input 
+                                id="school" 
+                                type="text" 
+                                value={school} 
+                                onChange={(e) => setSchool(e.target.value)} 
+                                placeholder="Enter school name" 
+                                className={`p-2 border rounded-lg ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-text'}`}
+                                disabled={!isEmailValid} // Disable input if the email is not valid
+                            />
+                        </div>
+    
+                        {/* Description */}
+                        <div className="flex flex-col">
+                            <label className="text-lg font-medium" htmlFor="description">Description</label>
+                            <textarea 
+                                id="description" 
+                                value={description} 
+                                onChange={(e) => setDescription(e.target.value)} 
+                                placeholder="Enter item description" 
+                                className={`p-2 border rounded-lg ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-text'}`}
+                                rows="4"
+                                disabled={!isEmailValid} // Disable input if the email is not valid
+                            />
+                        </div>
+    
+                        {/* Submit Button */}
+                        <div className="flex justify-center">
+                            <button 
+                                className={`bg-blue-500 text-white p-3 rounded-lg mt-4 ${!isEmailValid ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                                onClick={handleSubmit}
+                                disabled={!isEmailValid} // Disable the submit button if the email is not valid
+                            >
+                                Submit Listing
+                            </button>
+                        </div>
                     </div>
-
-                    {/* Submit Button */}
-                    <div className="flex justify-center">
-                        <button 
-                            className="bg-blue-500 text-white p-3 rounded-lg mt-4"
-                            onClick={handleSubmit}
-                        >
-                            Submit Listing
-                        </button>
-                    </div>
+    
                 </div>
-
             </div>
-        </div>
-    </>
+        </>
     );
+    
+    
 }
