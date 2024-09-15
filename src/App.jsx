f06c595b43c2f286d2e56d9d5e12193e2d5fa42e
Lauren Ji <lyangji1011@gmail.com>
2024-09-14 20:29:18 -0400

    something?

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect, useCallback } from "react";
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Browse from './pages/browse';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Browse from "./pages/browse";
 import "./App.css";
 import Navbar from "./components/navbar";
-import PostListing from './pages/postListing';
+import PostListing from "./pages/postListing";
+import Profile from "./pages/profile.jsx";
 import { allSchools } from "./schoolData.js";
-import { Input, InputGroup, InputRightAddon, Heading, Box, List, ListItem } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Heading,
+  Box,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
@@ -30,7 +39,7 @@ function App() {
   };
 
   const fetchSchoolSuggestions = (query) => {
-    return allSchools.filter(school =>
+    return allSchools.filter((school) =>
       school.toLowerCase().includes(query.toLowerCase())
     );
   };
@@ -41,45 +50,57 @@ function App() {
   };
 
   const handleSearch = () => {
-    console.log("search", school);  
-    navigate("/browse", { state: { school } }); 
+    console.log("search", school);
+    navigate("/browse", { state: { school } });
   };
 
   const handleClickOutside = useCallback((event) => {
-    if (!event.target.closest('.suggestions-box') && !event.target.closest('.chakra-input')) {
+    if (
+      !event.target.closest(".suggestions-box") &&
+      !event.target.closest(".chakra-input")
+    ) {
       setShowSuggestions(false);
     }
   }, []);
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [handleClickOutside]);
 
   return (
     <Routes>
-      <Route 
-        path="/" 
+      <Route
+        path="/"
         element={
-          <Home 
-            onSearch={handleSearch} 
-            onInputChange={handleInputChange} 
-            school={school} 
-            suggestions={suggestions} 
-            handleSelectSuggestion={handleSelectSuggestion} 
-            showSuggestions={showSuggestions} 
-          />} 
+          <Home
+            onSearch={handleSearch}
+            onInputChange={handleInputChange}
+            school={school}
+            suggestions={suggestions}
+            handleSelectSuggestion={handleSelectSuggestion}
+            showSuggestions={showSuggestions}
+          />
+        }
       />
       <Route path="/browse" element={<Browse />} />
       <Route path="/postListing" element={<PostListing />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
 
-function Home({ onSearch, onInputChange, school, suggestions, handleSelectSuggestion, showSuggestions }) {
+function Home({
+  onSearch,
+  onInputChange,
+  school,
+  suggestions,
+  handleSelectSuggestion,
+  showSuggestions,
+}) {
   return (
     <div>
       <div className="fixed top-0 left-0 w-full z-50">
@@ -90,7 +111,12 @@ function Home({ onSearch, onInputChange, school, suggestions, handleSelectSugges
           GradGoods
         </Heading>
         <div className="flex flex-row justify-center">
-          <InputGroup size="lg" width="45vw" className="mt-8" position="relative">
+          <InputGroup
+            size="lg"
+            width="45vw"
+            className="mt-8"
+            position="relative"
+          >
             <Input
               onChange={onInputChange}
               value={school}
@@ -100,7 +126,7 @@ function Home({ onSearch, onInputChange, school, suggestions, handleSelectSugges
                   onSearch();
                 }
               }}
-              className="chakra-input" 
+              className="chakra-input"
             />
             {showSuggestions && suggestions.length > 0 && (
               <Box
@@ -113,15 +139,15 @@ function Home({ onSearch, onInputChange, school, suggestions, handleSelectSugges
                 boxShadow="md"
                 zIndex="1"
                 mt="2"
-                maxH="200px" 
-                overflowY="auto" 
+                maxH="200px"
+                overflowY="auto"
               >
                 <List spacing={1}>
                   {suggestions.map((suggestion, index) => (
                     <ListItem
                       key={index}
                       padding="2"
-                      _hover={{ bg: 'gray.100', cursor: 'pointer' }}
+                      _hover={{ bg: "gray.100", cursor: "pointer" }}
                       onClick={() => handleSelectSuggestion(suggestion)}
                     >
                       {suggestion}
@@ -130,7 +156,10 @@ function Home({ onSearch, onInputChange, school, suggestions, handleSelectSugges
                 </List>
               </Box>
             )}
-            <InputRightAddon onClick={onSearch}>
+            <InputRightAddon
+              onClick={onSearch}
+              className="hover:cursor-pointer"
+            >
               <SearchIcon color="gray.600" />
             </InputRightAddon>
           </InputGroup>
