616f64531e0e1801e0c76c580f02e37ba9f7c4b2
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-09-15 01:05:26 -0400

    made delete fn

--- src/components/BrowseItemDisplay.jsx
+++ src/components/BrowseItemDisplay.jsx
@@ -2,15 +2,15 @@ import ItemCard from "./ItemCard";
 import { Wrap, WrapItem } from "@chakra-ui/react";
 
 export default function BrowseItemDisplay({ items }) {
-  if (!items) {
-    return <div>no items match your search</div>;
+  if (!items || items.length === 0) {
+    return <div>No items match your search.</div>;
   }
 
   return (
     <Wrap spacing={4}>
       {items.map((item) => {
         return (
-          <WrapItem>
+          <WrapItem key={item.id}>
             <ItemCard item={item} />
           </WrapItem>
         );
