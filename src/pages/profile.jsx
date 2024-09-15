99d37baa4754e836d6091ab1d7354827a0d5f62d
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-09-14 22:11:38 -0400

    adding active/inactive state

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -3,6 +3,8 @@ import { useUser } from "@clerk/clerk-react";
 import Navbar from "../components/navbar";
 import { Heading, Divider } from "@chakra-ui/react";
 import axios from 'axios';
+import placeholder from '../assets/placeholder.jpeg';
+import { use } from "framer-motion/client";
 
 export default function Profile() {
   const { isSignedIn, user, isLoaded } = useUser();
@@ -10,6 +12,9 @@ export default function Profile() {
   // Move the hooks to the top level of the component
   const [listings, setListings] = useState([]);
   const [userListings, setUserListings] = useState([]);
+  const [userInactiveListings, setUserInactiveListings] = useState([]);
+
+  const [active, setActive] = useState(true);
 
   useEffect(() => {
     if (isSignedIn && user) {
@@ -17,13 +22,17 @@ export default function Profile() {
         try {
           const response = await axios.get('http://127.0.0.1:8000/api/get/');
           const database = response.data;
-          console.log(database);
           if (user) {
             setListings(response.data);
             const userJoinedListings = database.filter(listing =>
               listing.owner.includes(user.username)
             );
             setUserListings(userJoinedListings);
+
+            const inactiveListings = userJoinedListings.filter(listing =>
+              listing.active.includes(false)
+            );
+            setUserInactiveListings(inactiveListings);
           }
         } catch (error) {
           console.error('Error fetching Data:', error);
@@ -42,6 +51,9 @@ export default function Profile() {
       fetchData();
     }
   }, [isSignedIn, user]); // Dependencies
+
+  useEffect(() => {
+  }, [active]);
 
   if (!isSignedIn) {
     return <div>Not signed in</div>;
@@ -88,7 +100,43 @@ export default function Profile() {
           </div>
         </div>
         <Divider />
-        <Heading>My Listings</Heading>
+        
+        <div className="flex flex-row w-full h-full py-16">
+            <div className="flex flex-row w-1/4 h-full">
+              <div className="flex flex-col space-y-2">
+                <Heading>My Listings</Heading>
+                <p onClick={() => setActive(!active)} className={`font-bold hover:cursor-pointer ${active ? 'text-black' : 'text-gray-500'}`}> Active </p>
+                <p onClick={() => setActive(!active)} className={`font-bold hover:cursor-pointer ${active ? 'text-gray-500' : 'text-black'}`}> Inactive </p>
+              </div>
+            </div>
+            <div className="flex flex-row w-3/4 h-full">
+              <div className='flex gap-6 items-center flex-wrap'>
+                {userListings.map((listing, index) => {
+                const imagesArray = listing.imageURLs ? listing.imageURLs.split(',') : []; // Split the 'images' string into an array
+                return ( 
+                  <div
+                    key={index}
+                    className='relative flex flex-col h-56 w-56 hover:cursor-pointer border border-gray-200'
+                  >
+                    <img
+                      src={!imagesArray[0] ? placeholder : imagesArray[0]}
+                      alt='Listing Image'
+                      className='h-1/2 w-full'
+                    />
+                    <div className='flex flex-col space-y-1 justify-start text-start px-4'>
+                      <div className='flex flex-col space-y-1'>
+                        <p className='text-xs'> {listing.itemCategory} </p>
+                        <h1 className='text-lg font-bold'> {listing.itemName} </h1>
+                        <p className='text-md'> ${listing.price} </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+              </div>
+            </div>
+        </div>
+        
       </div>
     </div>
   );
