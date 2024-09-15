d5b49c520bcc2cddddeeb6062f9fc7d3c280763c
Lauren Ji <lyangji1011@gmail.com>
2024-09-15 00:40:54 -0400

    temp

--- src/pages/listing.jsx
+++ src/pages/listing.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Heading, Button, Image, Icon } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import { useUser } from "@clerk/clerk-react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/navbar";
 
 export default function Listing() {
   const [listings, setListings] = useState([]);
+  const [currImage, setCurrImage] = useState(1);
+  const [imagesArr, setImagesArr] = useState([]);
   const { isSignedIn, user, isLoaded } = useUser();
   const { id } = useParams();
   const [curr, setCurr] = useState(null);
+  const [isMyListing, setIsMyListing] = useState(false);
 
   useEffect(() => {
     if (isSignedIn && user) {
@@ -41,14 +46,29 @@ export default function Listing() {
       const listing = listings.filter((l) => {
         return l.id.toString() === id;
       });
+      console.log(listing);
       setCurr(listing[0]);
-      console.log(listing[0]);
     }
   }, [listings]);
+
+  useEffect(() => {
+    if (curr) {
+      setImagesArr(curr.imageURLs.split(","));
+      console.log(curr.imageURLs.split(","));
+    }
+  }, [curr]);
 
   if (!curr) {
     return <div>does not exist</div>;
   }
+
+  const decreaseImageIndex = () => {
+    setCurrImage(currImage - 1);
+  };
+
+  const increaseImageIndex = () => {
+    setCurrImage(currImage + 1);
+  };
 
   return (
     <div>
@@ -56,14 +76,47 @@ export default function Listing() {
         <Navbar />
       </div>
       <div className="mt-20 mx-[5vw] flex flex-row justify-between">
-        <div className="w-[50%]">image</div>
+        <div className="w-[50%] flex flex-row justify-center items-center">
+          <Icon
+            boxSize={10}
+            cursor={currImage > 0 ? "pointer" : null}
+            as={ChevronLeftIcon}
+            mr="4"
+            onClick={currImage > 0 ? decreaseImageIndex : null}
+            color={currImage > 0 ? "gray.700" : "gray.300"}
+          />
+          <Image
+            boxSize="68%"
+            objectFit="cover"
+            src={imagesArr[currImage]}
+            alt="product-image"
+          />
+          <Icon
+            cursor={currImage < imagesArr.length - 1 ? "pointer" : null}
+            boxSize={10}
+            color={currImage < imagesArr.length - 1 ? "gray.700" : "gray.300"}
+            ml="4"
+            as={ChevronRightIcon}
+            onClick={
+              currImage < imagesArr.length - 1 > 0 ? increaseImageIndex : null
+            }
+          />
+        </div>
         <div className="w-[50%]">
-          <h1>{curr.itemName}</h1>
-          <p>{curr.owner}</p>
+          <Heading size="2xl">{curr.itemName}</Heading>
+          <p className="mb-4 text-2xl font-medium">{curr.owner}</p>
           {curr.itemCategory === "Clothing" || curr.itemCategory === "Shoes" ? (
-            <div>Size: </div>
+            <div className="mb-4 text-2xl font-normal">Size: </div>
           ) : null}
-          <p>Category: {curr.itemCategory}</p>
+          <p className="mb-4 text-xl font-normal">
+            Category: {curr.itemCategory}
+          </p>
+          <p className="mb-4 text-2xl font-bold">${curr.price}</p>
+          <Button size="lg" colorScheme="green">
+            Buy now
+          </Button>
+          <p className="mb-2 text-2xl font-bold mt-8">Description</p>
+          <p className="text-lg">{curr.description}</p>
         </div>
       </div>
     </div>
