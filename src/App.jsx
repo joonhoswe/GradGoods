4b1595033ac3cf1548ef68ea969d95af086ee509
kcnakamu <aquachat77@gmail.com>
2024-09-14 22:03:43 -0400

    Updated Home Page (will fix)

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar";
 import PostListing from './pages/postListing';
 import { Input, InputGroup, InputRightAddon, Heading } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import MyImage from './img/back.png';
 
 function App() {
   const [school, setSchool] = useState("");
@@ -37,20 +38,35 @@ export default App;
 // Home component inside App.jsx
 function Home({ onSearch, onInputChange, school }) {
   return (
-    <div>
+    // Background
+    <div style = {{ 
+      backgroundImage: `url(${MyImage}),url(${MyImage})`,
+      // backgroundSize: 'cover', 
+      // backgroundPosition: 'left',
+      backgroundPosition: 'left 270%, right 270%',
+      backgroundRepeat: 'no-repeat, no-repeat',
+      height: '100vh', 
+      width: '100vw', 
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'left'
+      }}>
+  {/* // <div style={{ backgroundImage: `url(${MyImage})`, backgroundSize: 'cover', backgroundPosition: 'center',}}> */}
       <div className="fixed top-0 left-0 w-full z-50">
         <Navbar />
       </div>
-      <div className="mt-[35vh]">
+      
+      <div className="mt-[35vh]" style = {{zIndex:2}}>
         <Heading className="text-center" as="h2" size="3xl" p={0} noOfLines={1}>
           GradGoods
         </Heading>
         <div className="flex flex-row justify-center">
-          <InputGroup size="lg" width="45vw" className="mt-8">
+          <InputGroup size="lg" width="45vw" className="mt-8" >
             <Input
               onChange={onInputChange}
               value={school}
               placeholder="Search for your school"
+              style={{ backgroundColor: '#f0f0f0'}}
               onKeyDown={(e) => {
                 if (e.key === "Enter" || e.key === "Return") {
                   onSearch();
@@ -58,7 +74,7 @@ function Home({ onSearch, onInputChange, school }) {
               }}
             />
             <InputRightAddon onClick={onSearch}>
-              <SearchIcon color="gray.600" />
+              <SearchIcon color="black.600" />
             </InputRightAddon>
           </InputGroup>
         </div>
